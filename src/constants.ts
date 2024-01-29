0635ca0c47791994a421e744e0d8d1d4093f263d
Juan Peñalba <juantandil123@gmail.com>
2024-01-29 01:39:27 -0300

    pagina galeria creada

---
+++ src/constants.ts
@@ -0,0 +1,17 @@
+export const TRABAJOS_LIST = [
+  {
+    title: "Gastronomia",
+  },
+  {
+    title: "Bandas",
+  },
+  {
+    title: "Videos",
+  },
+  {
+    title: "Retratos",
+  },
+  {
+    title: "Analogico",
+  },
+];
